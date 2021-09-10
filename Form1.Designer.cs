4945ba41ec4bab94edf75c5d2eebf7cae5b8e59a
karan0550 <69427163+karan0550@users.noreply.github.com>
2021-09-09 23:21:22 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myImage1 = new System.Windows.Forms.PictureBox();
+            this.myImage2 = new System.Windows.Forms.PictureBox();
+            this.myImage3 = new System.Windows.Forms.PictureBox();
+            this.myImage4 = new System.Windows.Forms.PictureBox();
+            this.myImage5 = new System.Windows.Forms.PictureBox();
+            this.myImage6 = new System.Windows.Forms.PictureBox();
+            this.myImage7 = new System.Windows.Forms.PictureBox();
+            this.myImage8 = new System.Windows.Forms.PictureBox();
+            this.myImage9 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myImage1
+            // 
+            this.myImage1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage1.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage1.Location = new System.Drawing.Point(181, 108);
+            this.myImage1.Name = "myImage1";
+            this.myImage1.Size = new System.Drawing.Size(154, 149);
+            this.myImage1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage1.TabIndex = 0;
+            this.myImage1.TabStop = false;
+            this.myImage1.Click += new System.EventHandler(this.myImage1_Click);
+            // 
+            // myImage2
+            // 
+            this.myImage2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage2.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage2.Location = new System.Drawing.Point(341, 108);
+            this.myImage2.Name = "myImage2";
+            this.myImage2.Size = new System.Drawing.Size(154, 149);
+            this.myImage2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage2.TabIndex = 0;
+            this.myImage2.TabStop = false;
+            this.myImage2.Click += new System.EventHandler(this.myImage2_Click);
+            // 
+            // myImage3
+            // 
+            this.myImage3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage3.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage3.Location = new System.Drawing.Point(501, 108);
+            this.myImage3.Name = "myImage3";
+            this.myImage3.Size = new System.Drawing.Size(154, 149);
+            this.myImage3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage3.TabIndex = 0;
+            this.myImage3.TabStop = false;
+            this.myImage3.Click += new System.EventHandler(this.myImage3_Click);
+            // 
+            // myImage4
+            // 
+            this.myImage4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage4.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage4.Location = new System.Drawing.Point(181, 263);
+            this.myImage4.Name = "myImage4";
+            this.myImage4.Size = new System.Drawing.Size(154, 149);
+            this.myImage4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage4.TabIndex = 0;
+            this.myImage4.TabStop = false;
+            this.myImage4.Click += new System.EventHandler(this.myImage4_Click);
+            // 
+            // myImage5
+            // 
+            this.myImage5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage5.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage5.Location = new System.Drawing.Point(341, 263);
+            this.myImage5.Name = "myImage5";
+            this.myImage5.Size = new System.Drawing.Size(154, 149);
+            this.myImage5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage5.TabIndex = 0;
+            this.myImage5.TabStop = false;
+            this.myImage5.Click += new System.EventHandler(this.myImage5_Click);
+            // 
+            // myImage6
+            // 
+            this.myImage6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage6.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage6.Location = new System.Drawing.Point(501, 263);
+            this.myImage6.Name = "myImage6";
+            this.myImage6.Size = new System.Drawing.Size(154, 149);
+            this.myImage6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage6.TabIndex = 0;
+            this.myImage6.TabStop = false;
+            this.myImage6.Click += new System.EventHandler(this.myImage6_Click);
+            // 
+            // myImage7
+            // 
+            this.myImage7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage7.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage7.Location = new System.Drawing.Point(181, 418);
+            this.myImage7.Name = "myImage7";
+            this.myImage7.Size = new System.Drawing.Size(154, 149);
+            this.myImage7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage7.TabIndex = 0;
+            this.myImage7.TabStop = false;
+            this.myImage7.Click += new System.EventHandler(this.myImage7_Click);
+            // 
+            // myImage8
+            // 
+            this.myImage8.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage8.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage8.Location = new System.Drawing.Point(341, 418);
+            this.myImage8.Name = "myImage8";
+            this.myImage8.Size = new System.Drawing.Size(154, 149);
+            this.myImage8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage8.TabIndex = 0;
+            this.myImage8.TabStop = false;
+            this.myImage8.Click += new System.EventHandler(this.myImage8_Click);
+            // 
+            // myImage9
+            // 
+            this.myImage9.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.myImage9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.myImage9.Image = global::TicTacToe.Properties.Resources.tile;
+            this.myImage9.Location = new System.Drawing.Point(501, 418);
+            this.myImage9.Name = "myImage9";
+            this.myImage9.Size = new System.Drawing.Size(154, 149);
+            this.myImage9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.myImage9.TabIndex = 0;
+            this.myImage9.TabStop = false;
+            this.myImage9.Click += new System.EventHandler(this.myImage9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(62, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(1011, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1066, 718);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.myImage9);
+            this.Controls.Add(this.myImage6);
+            this.Controls.Add(this.myImage3);
+            this.Controls.Add(this.myImage8);
+            this.Controls.Add(this.myImage7);
+            this.Controls.Add(this.myImage5);
+            this.Controls.Add(this.myImage4);
+            this.Controls.Add(this.myImage2);
+            this.Controls.Add(this.myImage1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.myImage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myImage9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox myImage1;
+        private System.Windows.Forms.PictureBox myImage2;
+        private System.Windows.Forms.PictureBox myImage3;
+        private System.Windows.Forms.PictureBox myImage4;
+        private System.Windows.Forms.PictureBox myImage5;
+        private System.Windows.Forms.PictureBox myImage6;
+        private System.Windows.Forms.PictureBox myImage7;
+        private System.Windows.Forms.PictureBox myImage8;
+        private System.Windows.Forms.PictureBox myImage9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,346 @@
+﻿/** 
+ * Project name: KSharmaAssignment1
+ * 
+ * Purpose: To build a game TicTacToe
+ * 
+ * Revision History: 
+ *                  Created by Karan Sharma.
+ * **/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool player = true;
+        int[,] array = new int[3, 3];
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void myImage1_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+            if (player == true)
+            {
+                array[0, 0] = 1;
+            }
+            else
+            {
+                array[0, 0] = -1;
+            }
+            ChangeImage(myImage1, label1);
+            if (count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage2_Click(object sender, EventArgs e)
+        {
+            count++;
+            if (player == true)
+            {
+                //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+                array[0, 1] = 1;
+            }
+            else
+            {
+                array[0, 1] = -1;
+            }
+            ChangeImage(myImage2, label1);
+            if (count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage3_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+            if (player == true)
+            {
+                array[0, 2] = 1;
+            }
+            else
+            {
+                array[0, 2] = -1;
+            }
+            ChangeImage(myImage3, label1);
+            if (count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage4_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+            if (player == true)
+            {
+                array[1, 0] = 1;
+            }
+            else
+            {
+                array[1, 0] = -1;
+            }
+            ChangeImage(myImage4, label1);
+            if (count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage5_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+            if (player == true)
+            {
+                array[1, 1] = 1;
+            }
+            else
+            {
+                array[1, 1] = -1;
+            }
+            ChangeImage(myImage5, label1);
+            if (count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage6_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+            if (player == true)
+            {
+                array[1, 2] = 1;
+            }
+            else
+            {
+                array[1, 2] = -1;
+            }
+            ChangeImage(myImage6, label1);
+            if(count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage7_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+            if (player == true)
+            {
+                array[2, 0] = 1;
+            }
+            else
+            {
+                array[2, 0] = -1;
+            }
+            ChangeImage(myImage7, label1);
+            if (count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage8_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B.
+            if (player == true)
+            {
+                array[2, 1] = 1;
+            }
+            else
+            {
+                array[2, 1] = -1;
+            }
+            ChangeImage(myImage8, label1);
+            if (count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        private void myImage9_Click(object sender, EventArgs e)
+        {
+            count++;
+            //Giving Indexes and values to the indexes for the picture boxes. The value will be 1 for player A and -1 for player B. 
+            if (player == true)
+            {
+                array[2, 2] = 1;
+            }
+            else
+            {
+                array[2, 2] = -1;
+            }
+            ChangeImage(myImage9, label1);
+            if(count > 4)
+            {
+                CheckLogic();
+            }
+        }
+
+        /// <summary>
+        /// This method changes the picture in the picturebox when clicked, also on the top left the label changes displaying the player's turns.
+        /// </summary>
+        /// <param name="pictureBox">Parameer used to add and change pictures on picturebox depending on player's turns</param>
+        /// <param name="label">To change the label showing each player's turn.</param>
+        public void ChangeImage(PictureBox pictureBox, Label label)
+        {
+            if (player == true)
+            {
+                pictureBox.Image = Image.FromFile(@"C:\Conestoga\Level 3\Game Prog Data Structures- Prog2370\Assignments\KSharmaAssignment1\Resources\cross.jpg");
+                player = false;
+                label.Text = "Player A";
+            }
+            else
+            {
+                pictureBox.Image = Image.FromFile(@"C:\Conestoga\Level 3\Game Prog Data Structures- Prog2370\Assignments\KSharmaAssignment1\Resources\O.jpg");
+                player = true;
+                label.Text = "Player B";
+            }
+            pictureBox.Enabled = false;
+        }
+
+        /// <summary>
+        /// This method checks the sum of the rows, columns and diagonals to decide the winner. If the sum is 3 then player A wins and if sum is -3 then player B wins. 
+        /// </summary>
+        public void CheckLogic()
+        {
+            int[] row = new int[3];
+            int[] col = new int[3];
+            int diagonal1 = 0;
+            int diagonal2 = 0;
+            //nested loops to find the sum of rows, columns and both the diagonals.
+            //loop to check rows
+            for(int i = 0; i < 3; i++)
+            {
+                row[i] = 0;
+                col[i] = 0;
+                //loop to check columns
+                for(int j = 0; j < 3; j++)
+                {
+                    row[i] += array[i, j];
+                    col[i] += array[j, i];
+                    if(i == j)
+                    {
+                        diagonal1 += array[i, j]; 
+                    }
+                    if(i + j == 2)
+                    {
+                        diagonal2 += array[i, j];
+                    }
+                }
+            }
+            //Statements to check the values of the sum, if 3 then player A wins and if -3 then player B.
+            if(diagonal1 == 3 || diagonal2 == 3)
+            {
+                DialogResult result = MessageBox.Show("X won! Play again?", "Game Over", MessageBoxButtons.YesNo);
+                if(result == DialogResult.Yes)
+                {
+                    Form1 obj = new Form1();
+                    obj.Show();
+                    this.Hide();
+                }
+                if(result == DialogResult.No)
+                {
+                    this.Close();
+                }
+                return;
+            }
+            else if(diagonal1 == -3 || diagonal2 == -3)
+            {
+                DialogResult result = MessageBox.Show("O won! Play again?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Form1 obj = new Form1();
+                    obj.Show();
+                    this.Hide();
+                }
+                if (result == DialogResult.No)
+                {
+                    this.Close();
+                }
+                return;
+            }
+            else
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    if(row[i] == 3 || col[i] == 3)
+                    {
+                        DialogResult result = MessageBox.Show("X won! Play again?", "Game Over", MessageBoxButtons.YesNo);
+                        if (result == DialogResult.Yes)
+                        {
+                            Form1 obj = new Form1();
+                            obj.Show();
+                            this.Hide();
+                        }
+                        if (result == DialogResult.No)
+                        {
+                            this.Close();
+                        }
+                        return;
+                    }
+                    else if(col[i] == -3 || row[i] == -3)
+                    {
+                        DialogResult result = MessageBox.Show("O won! Play again?", "Game Over", MessageBoxButtons.YesNo);
+                        if (result == DialogResult.Yes)
+                        {
+                            Form1 obj = new Form1();
+                            obj.Show();
+                            this.Hide();
+                        }
+                        if (result == DialogResult.No)
+                        {
+                            this.Close();
+                        }
+                        return;
+                    }
+                }
+            }
+            //statement to check if the game is draw
+            if(count == 9)
+            {
+                DialogResult result = MessageBox.Show("The game is Draw! Play again?", "Game Over", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Form1 obj = new Form1();
+                    obj.Show();
+                    this.Hide();
+                }
+                if (result == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
